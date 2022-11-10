e30864e8912d6c923de45c0e1c9a623f4333f571
sebachu789 <117925902+sebachu789@users.noreply.github.com>
2022-11-10 12:09:23 -0300

    Add files via upload

---
+++ pruebasitio/scripts/main.js
@@ -0,0 +1,41 @@
+function multiplica(num1,num2) {
+  let resultado = num1 * num2;
+  return resultado;
+}
+
+
+let miImage = document.querySelector('img');
+miImage.onclick = function () {
+    let miSrc = miImage.getAttribute('src');
+    if (miSrc === 'images/Nissan_Skyline_R32_GT-R_001.jpg') {
+      miImage.setAttribute('src','images/Nissan_Skyline_R32_GT-R_002.jpg');
+    } else {
+      miImage.setAttribute('src', 'images/Nissan_Skyline_R32_GT-R_001.jpg');
+    }
+}
+
+let miBoton = document.querySelector('button');
+let miTitulo = document.querySelector( 'h1');
+
+function estableceNombreUsuario() {
+  let miNombre = prompt('Introduzca su nombre.');
+  if(!miNombre) {
+    estableceNombreUsuario();
+  } else {
+    localStorage.setItem('nombre', miNombre);
+    miTitulo.innerHTML = 'Mozilla is genial, ' + miNombre;
+  }
+}
+
+
+if (!localStorage.getItem('nombre')) {
+    estableceNombreUsuario();
+}
+else {
+    let nombreAlmacenado = localStorage.getItem('nombre');
+    miTitulo.textContent = 'Mozilla es genial,' + nombreAlmacenado;
+}
+
+miBoton.onclick = function() {
+    estableceNombreUsuario();
+}
